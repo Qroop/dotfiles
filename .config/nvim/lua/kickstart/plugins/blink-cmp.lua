f22f364373dbc3c2c7ca7059264ff86641c636ce
Jesper Wallengren <jesper.wallengren@gmail.com>
2025-07-05 18:16:44 +0200

    Switched to gruvbox

--- .config/nvim/lua/kickstart/plugins/blink-cmp.lua
+++ .config/nvim/lua/kickstart/plugins/blink-cmp.lua
@@ -21,12 +21,12 @@ return {
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
         },
         opts = {},
       },
@@ -61,6 +61,9 @@ return {
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+        ['<enter>'] = { 'select_and_accept', 'fallback' },
+        ['<C-j>'] = { 'select_next', 'fallback' },
+        ['<C-k>'] = { 'select_prev', 'fallback' },
       },
 
       appearance = {
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,6 +4,8 @@ local config = {
 	color_scheme = "Gruvbox Dark (Gogh)",
 	window_background_opacity = 0.8,
 	window_decorations = "RESIZE",
+	window_padding = { left = "0cell", right = "0cell", top = "0cell", bottom = "0cell" },
+	hide_tab_bar_if_only_one_tab = true,
 }
 
 return config
