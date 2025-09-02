d216fab01e2a97c1b2bc44d5a61ec0200cfb4000
Jesper Wallengren <jesper.wallengren@gmail.com>
2025-09-02 09:42:32 +0200

    Add better handling of swedish coder layout

--- install/swe-coder-layout.sh
+++ install/swe-coder-layout.sh
@@ -1,21 +1,5 @@
 #!/usr/bin/env bash
 
-distro_switch_case () {
-    case "$DISTRO_LIKE" in
-        debian)
-            echo ">> Updating xkb-data"
-            sudo dpkg-reconfigure xkb-data
-        ;;
-        arch)
-            echo ">> No xkb-data update needed on Arch"
-        ;;
-        *)
-            echo ">> No known distro detected, exiting"
-            return 1
-        ;;
-    esac
-}
-
 main () {
     set -e
 
@@ -28,33 +12,10 @@ main () {
     echo ">> Installing Swedish (Coder) keyboard layout variant"
 
     SYMBOLS_FILE="/usr/share/X11/xkb/symbols/se"
-    VARIANT_NAME="coder"
-    LAYOUT_MARKER="xkb_symbols \"$VARIANT_NAME\""
-
-    if ! grep -q "$LAYOUT_MARKER" "$SYMBOLS_FILE"; then
-        echo ">> Appending custom variant to $SYMBOLS_FILE"
-        sudo bash -c "cat \"$PWD/../xkb/se-coder.xkb\" >> \"$SYMBOLS_FILE\""
-    else
-        echo ">> Variant already present, skipping patch."
-    fi
-
-    if ! distro_switch_case; then
-        return
-    fi
-    
-    echo ">> Making layout persistent using localectl"
-
-    if [[ "$XDG_CURRENT_DESKTOP" == "KDE" && "$XDG_SESSION_TYPE" == "wayland" ]]; then
-        echo ">> Detected KDE on Wayland."
-        echo ">> Use System Settings > Input Devices > Layouts"
-        echo "   to select Swedish layout with 'coder' variant to persist it."
-        return
-    fi
-
-    echo ">> Done. Set layout with: setxkbmap se coder"
-    setxkbmap se coder
-
-    sudo localectl set-x11-keymap se $VARIANT_NAME
+	echo ">> Symlinking layout"
+	sudo rm -f $SYMBOLS_FILE
+	sudo ln -s "$DOTFILES_DIR/static/kde-settings/se" $SYMBOLS_FILE
+	echo ">> Done symlinking keyboard layout, remember to change to the Swedish US layout"
 }
 
 main
