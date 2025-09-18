fe49f09bf79d9fabe572048bfe1c6829535ed3dc
Jesper Wallengren <jesper.wallengren@gmail.com>
2025-09-18 17:35:37 +0200

    Fix neovim clipboard

--- zsh/alias.sh
+++ zsh/alias.sh
@@ -2,3 +2,9 @@ alias cat='bat'
 alias nv='n'
 alias dotnv="n ~/.dotfiles"
 alias e++17='g++ -std=c++17 -Wall -Wextra -pedantic -Weffc++ -Wold-style-cast'
+alias gll='~/Upp/getlab-cli/staff_cli.py ls tdde71_ht25'
+alias glg='~/Upp/getlab-cli/staff_cli.py get tdde71_ht25'
+alias gld='~/Upp/getlab-cli/staff_cli.py done tdde71_ht25'
+alias getlab='~/Upp/getlab-cli/staff_cli.py'
+
+
