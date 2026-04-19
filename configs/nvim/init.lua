f1fd24faa18cca58cb1df83b4424dd9735f2844f
Jesper Wallengren <jesper.wallengren@gmail.com>
2026-04-19 19:57:37 +0200

    Refactor to use native package nvim manager

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -1,72 +1,32 @@
 ---@diagnostic disable: undefined-global, unused-local
 vim.g.mapleader = ' '
--- Clone 'mini.nvim' manually in a way that it gets managed by 'mini.deps'
-local path_package = vim.fn.stdpath('data') .. '/site/'
-local mini_path = path_package .. 'pack/deps/start/mini.nvim'
-if not vim.loop.fs_stat(mini_path) then
-	vim.cmd('echo "Installing [`mini.nvim`](../doc/mini-nvim.qmd#mini.nvim)" | redraw')
-	local clone_cmd = {
-		'git', 'clone', '--filter=blob:none',
-		'https://github.com/nvim-mini/mini.nvim', mini_path
-	}
-	vim.fn.system(clone_cmd)
-	vim.cmd('packadd mini.nvim | helptags ALL')
-	vim.cmd('echo "Installed [`mini.nvim`](../doc/mini-nvim.qmd#mini.nvim)" | redraw')
-end
 
 -- OPTIONS
 vim.o.winborder = 'rounded'
-
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.mouse = 'a'
 vim.o.clipboard = 'unnamedplus'
 vim.o.showmode = false
 vim.o.wrap = false
-
--- Enable break indent
 vim.o.breakindent = true
-
--- Save undo history
 vim.o.undofile = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.o.ignorecase = true
 vim.o.smartcase = true
-
--- Keep signcolumn on by default
 vim.o.signcolumn = 'yes'
-
--- Decrease update time
 vim.o.updatetime = 250
-
 vim.o.timeoutlen = 1000
-
--- Configure how new splits should be opened
 vim.o.splitright = true
-
 vim.o.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Preview substitutions live, as you type!
 vim.o.inccommand = 'split'
-
--- Show which line your cursor is on
 vim.o.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
 vim.o.scrolloff = 8
-
--- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s)
--- See `:help 'confirm'`
 vim.o.confirm = true
-
 vim.opt.expandtab = false -- Use tabs, not spaces
 vim.opt.tabstop = 4       -- Display width of a tab character
 vim.opt.shiftwidth = 4    -- Indent/outdent by 4 spaces (or 1 tab in this case)
 vim.opt.softtabstop = 4   -- Makes <Tab> and <BS> behave like 4-space wide tabs
-
 vim.opt.swapfile = false
 
 vim.api.nvim_create_autocmd('TextYankPost', {
@@ -79,13 +39,21 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 
 -- PLUGINS
--- Use 'mini.deps'. `now()` and `later()` are helpers for a safe two-stage
--- Set up 'mini.deps' (customize to your liking)
-require('mini.deps').setup({ path = { package = path_package } })
-local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
-require('mini.extra').setup()      -- https://nvim-mini.org/mini.nvim/doc/mini-pairs.html
--- GRUVBOX
-add({ source = 'ellisonleao/gruvbox.nvim', })
+vim.pack.add({
+	"https://github.com/ellisonleao/gruvbox.nvim",
+	"https://github.com/nvim-mini/mini.extra",
+	"https://github.com/stevearc/oil.nvim",
+	"https://github.com/mason-org/mason.nvim",
+	"https://github.com/mason-org/mason-lspconfig.nvim",
+	"https://github.com/neovim/nvim-lspconfig",
+	"https://github.com/nvim-mini/mini.snippets",
+	"https://github.com/nvim-mini/mini.completion",
+	"https://github.com/nvim-mini/mini.pairs",
+	"https://github.com/nvim-mini/mini.clue",
+	"https://github.com/nvim-mini/mini.pick",
+	"https://github.com/nvim-mini/mini.notify",
+})
+
 require('gruvbox').setup {
 	terminal_colors = true,
 	transparent_mode = true,
@@ -99,7 +67,6 @@ vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#282828" })
 vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#282828", fg = "#928374" })
 vim.api.nvim_set_hl(0, "FloatTitle", { bg = "#282828" })
 
-add({ source = 'stevearc/oil.nvim' })
 require('oil').setup({
 	columns = {
 		"permissions",
@@ -118,10 +85,6 @@ require('oil').setup({
 vim.keymap.set('n', '<leader>e', ':Oil<CR>', { desc = 'Open file explorer' })
 
 -- LSP
-add({ source = 'mason-org/mason.nvim' })
-add({ source = 'mason-org/mason-lspconfig.nvim' })
-add({ source = 'neovim/nvim-lspconfig' })
-
 require("mason").setup()
 require("mason-lspconfig").setup({
 	ensure_installed = { "lua_ls", "pylsp" },
@@ -133,17 +96,12 @@ vim.lsp.enable({
 	"pylsp"
 })
 
-vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, { desc = 'Code actions' })
-vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = 'Goto implementation' })
-vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, { desc = 'Goto type defintion' })
-vim.keymap.set('n', 'gn', vim.lsp.buf.rename, { desc = 'Rename symbol' })
-vim.keymap.set('n', 'gf', vim.lsp.buf.format, { desc = 'Format file' })
-
 -- MINI
-require('mini.snippets').setup()   -- https://nvim-mini.org/mini.nvim/doc/mini-snippets.html
-require('mini.completion').setup() -- https://nvim-mini.org/mini.nvim/doc/mini-completion.html
-require('mini.pairs').setup()      -- https://nvim-mini.org/mini.nvim/doc/mini-pairs.html
-require('mini.clue').setup({       -- https://nvim-mini.org/mini.nvim/doc/mini-clue.html
+require('mini.extra').setup()
+require('mini.snippets').setup()
+require('mini.completion').setup()
+require('mini.pairs').setup()
+require('mini.clue').setup({
 	triggers = {
 		{
 			mode = 'n',
@@ -159,18 +117,13 @@ require('mini.clue').setup({       -- https://nvim-mini.org/mini.nvim/doc/mini-c
 	}
 })
 
--- https://nvim-mini.org/mini.nvim/doc/mini-pick.html
 require('mini.pick').setup({
 	mappings = {
 		move_down = '<C-j>',
 		move_up = '<C-k>',
 	}
 })
-vim.keymap.set('n', '<leader>ff', ':Pick files<CR>', { desc = 'Find files' })
-vim.keymap.set('n', '<leader>fb', ':Pick buffers<CR>', { desc = 'Find buffers' })
-vim.keymap.set('n', '<leader>fg', ':Pick grep_live<CR>', { desc = 'Find with grep' })
-vim.keymap.set('n', '<leader>fc', ':Pick resume<CR>', { desc = 'Continue last picker' })
-vim.keymap.set('n', '<leader>h', ':Pick help<CR>', { desc = 'Find help' })
+
 require('mini.notify').setup({
 	lsp_progress = {
 		enable = false,
@@ -178,6 +131,19 @@ require('mini.notify').setup({
 })
 
 -- KEYMAPS
+vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, { desc = 'Code actions' })
+vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = 'Goto implementation' })
+vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, { desc = 'Goto type defintion' })
+vim.keymap.set('n', 'gn', vim.lsp.buf.rename, { desc = 'Rename symbol' })
+vim.keymap.set('n', 'gf', vim.lsp.buf.format, { desc = 'Format file' })
+
+vim.keymap.set('n', '<leader>ff', ':Pick files<CR>', { desc = 'Find files' })
+vim.keymap.set('n', '<leader><leader>', ':Pick files<CR>', { desc = 'Find files' })
+vim.keymap.set('n', '<leader>fb', ':Pick buffers<CR>', { desc = 'Find buffers' })
+vim.keymap.set('n', '<leader>fg', ':Pick grep_live<CR>', { desc = 'Find with grep' })
+vim.keymap.set('n', '<leader>fc', ':Pick resume<CR>', { desc = 'Continue last picker' })
+vim.keymap.set('n', '<leader>h', ':Pick help<CR>', { desc = 'Find help' })
+
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('i', '<C-j>', '<C-n>', { noremap = true })
 vim.keymap.set('i', '<C-k>', '<C-p>', { noremap = true })
@@ -202,7 +168,6 @@ vim.keymap.set('n', '<leader>-', '<Cmd>split<CR>', { desc = '[-] Horizontal spli
 
 vim.keymap.set('n', '<leader>w', '<Cmd>set wrap!<CR>', { desc = 'Toggle wrap' })
 
--- Center screen after jumping
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', 'n', 'nzz')
@@ -217,7 +182,8 @@ pcall(vim.keymap.del, "n", "gri")
 pcall(vim.keymap.del, "n", "grn")
 pcall(vim.keymap.del, "n", "grr")
 pcall(vim.keymap.del, "n", "grt")
+pcall(vim.keymap.del, "n", "grx")
 
 vim.keymap.set('n', 'gr', function()
-  MiniExtra.pickers.lsp({ scope = 'references' })
+	MiniExtra.pickers.lsp({ scope = 'references' })
 end, { noremap = true, desc = 'Goto references' })
