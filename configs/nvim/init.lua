e3f0f15814e09bb58449573b9c84f4065edba71d
Jesper Wallengren <jesper.github@pm.me>
2026-06-14 23:43:13 +0200

    Remove help taking up entire window

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -17,7 +17,7 @@ vim.o.signcolumn = 'yes'
 vim.o.updatetime = 250
 vim.o.timeoutlen = 1000
 vim.o.splitright = true
-vim.o.splitbelow = true
+vim.o.splitbelow = false
 vim.o.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.o.inccommand = 'split'
@@ -150,11 +150,12 @@ require('mini.pick').setup({
 		content_from_bottom = true,
 	},
 	mappings = {
-		move_down = '<C-j>',
-		move_up = '<C-k>',
-		choose_in_split = '-',
-		choose_in_vsplit = '|',
-		scroll_left = '<C-q>',
+		move_down         = '<C-j>',
+		move_up           = '<C-k>',
+		choose_in_split   = '<C-s>',
+		choose_in_tabpage = '<C-t>',
+		choose_in_vsplit  = '<C-v>',
+		scroll_left       = '<C-q>',
 	},
 	window = {
 		config = function()
@@ -230,12 +231,3 @@ pcall(vim.keymap.del, "n", "grx")
 vim.keymap.set('n', 'gr', function()
 	MiniExtra.pickers.lsp({ scope = 'references' })
 end, { noremap = true, desc = 'Goto references' })
-
-vim.api.nvim_create_autocmd("BufWinEnter", {
-	pattern = { "*.txt" },
-	callback = function()
-		if vim.bo.filetype == "help" then
-			vim.cmd("only")
-		end
-	end,
-})
